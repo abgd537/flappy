58aced463493af79c143f0be2a17c39cf54268d6
abgd <90244110+abgd537@users.noreply.github.com>
2021-12-21 01:47:33 +0900

    Revert "v1.1"

--- src/main/KeyInput.kt
+++ src/main/KeyInput.kt
@@ -2,7 +2,6 @@ package main
 
 import java.awt.event.KeyAdapter
 import java.awt.event.KeyEvent
-import java.io.File
 import kotlin.system.exitProcess
 
 class KeyInput(private val game : Game) : KeyAdapter()
@@ -12,11 +11,6 @@ class KeyInput(private val game : Game) : KeyAdapter()
 		when(e.keyCode)
 		{
 			KeyEvent.VK_ESCAPE -> exitProcess(0)
-			
-			KeyEvent.VK_DELETE -> {
-				File("src/data/high").writeText("0")
-				game.handler.highScore = 0
-			}
 			
 			else -> {
 				if(!game.gameOver)
